58ce6c88b7bba495626838f660c41af5e844cfcd
Roy <rxlisbest@gmail.com>
2018-10-09 14:59:19 +0800

    migration

--- src/main/java/com/ishop/api/Controller/GoodsController.java
+++ src/main/java/com/ishop/api/Controller/GoodsController.java
@@ -7,15 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 import com.alibaba.fastjson.JSON;
 import com.ishop.api.bean.GoodsBean;
 import com.ishop.api.dao.GoodsMapper;
+
 import java.util.List;
 
 @RestController
 public class GoodsController {
     @Autowired
     private GoodsMapper goodsMapper;
-    @RequestMapping("/")
-    public String index(){
 
+    @RequestMapping("/")
+    public String index() {
         List<GoodsBean> goods = goodsMapper.select();
         String goods2 = JSON.toJSONString(goods);
         return goods2;
---
+++ src/main/resources/db/migration/V1.0__Goods.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `ishop_goods` (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  username varchar(100) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
