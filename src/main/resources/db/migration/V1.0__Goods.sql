e0dc5149af214c231408e788a7e52582230a6f84
Roy <rxlisbest@gmail.com>
2018-10-09 21:17:10 +0800

    数据库表设计

--- src/main/resources/db/migration/V1.0__Goods.sql
+++ src/main/resources/db/migration/V1.0__Goods.sql
@@ -1,5 +1,10 @@
 CREATE TABLE `ishop_goods` (
-  id bigint(20) NOT NULL AUTO_INCREMENT,
-  username varchar(100) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '名称',
+  `goods_category_id` varchar(255) NOT NULL DEFAULT '' COMMENT '名称',
+  `price` decimal(11, 2) NOT NULL DEFAULT 0.00 COMMENT '价格',
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '状态',
+  `create_time` bigint(20) NOT NULL DEFAULT 0,
+  `update_time` bigint(20) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='商品表';
